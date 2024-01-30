c987292639dba06c96fa6dfeb559efc1c5c136fa
unknown <rlgusrlcks00@gmail.com>
2024-01-30 14:55:32 +0900

    kichan

--- jsStudy/app.js
+++ jsStudy/app.js
@@ -1,42 +1,12 @@
-const title = document.getElementById("title");
-console.dir(title);
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+console.log(loginInput);
+console.log(loginButton);
 
-title.innerText = "This is change as JS";
-
-function handleTitleClick() {
-  title.style.color = "blue";
+function clickButton() {
+  const username = loginInput.value;
+  console.log(username);
+  alert(username);
 }
-title.addEventListener("click", handleTitleClick);
-
-function handleMouseEvent() {
-  title.style.color = "red";
-}
-title.addEventListener("mouseenter", handleMouseEvent);
-
-function handleLeaveMouseEvent() {
-  title.style.color = "black";
-}
-title.addEventListener("mouseleave", handleLeaveMouseEvent);
-//___________________________________________________________________
-
-const title2 = document.querySelector(".title2");
-console.dir(title2);
-
-function handleEnterMouse() {
-  title2.innerText = "WOw!";
-}
-
-function handleLeaveMouse() {
-  title2.innerText = "OH!";
-}
-title2.addEventListener("mouseenter", handleEnterMouse);
-title2.addEventListener("mouseleave", handleLeaveMouse);
-
-title2.onclick = handleTitleClick;
-
-//__________________________________
-
-function handleWindowCopy(){
-    alert("copier");
-}
-title2.addEventListener("copy", handleWindowCopy);
+loginForm.addEventListener("submit" , clickButton);
