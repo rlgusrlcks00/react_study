49fb4e775d82a7d8916bf76eb30f5294f995552c
unknown <rlgusrlcks00@gmail.com>
2024-02-01 16:44:01 +0900

    done

--- thisistest/src/App.js
+++ thisistest/src/App.js
@@ -5,11 +5,10 @@ function App() {
   const[counter, setValue] = useState(0);
   const[keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('run all the time');
   const iRunOnlyOnce = () =>{
     console.log("i run only once.");
   };
-
+  
   const onChange = (event) => {
     setKeyword(event.target.value);
   };
@@ -20,13 +19,32 @@ function App() {
     console.log("counter is" + counter);
   },[counter]);
   // useEffect(iRunOnlyOnce, []);
+
+  const [show, setShow] = useState(false);
+
+  const onClickShow = () =>{
+    setShow((prev) => !prev);
+  };
+  function Hello() {
+    useEffect(()=>{
+      console.log("hi WW");
+      return ()=>{
+        console.log("bye TT");
+      }
+    }, [])
+    return <h1>Hello</h1>;
+  };
+
   return (
     <div>
       <input onChange={onChange} type="text" placeholder="Search here..." value={keyword}/>
       <h2>{keyword}</h2>
       <h1 className= {styles.title}>Welcome</h1>
-      <Button text={"Continue"}/>
+      <Button onClick ={Hello} text={"Continue"}/>
       <button onClick={onClick}>Click me!</button>
+      <button onClick={onClickShow}>{show ? "Hide":"Show"}</button>
+      {show ? <Hello /> : null}
+      <h4>{show ? "Is this ShOW?": "This is SHOW"}</h4>
       <h4>This is vlaue : {counter}</h4>
     </div>
   );
